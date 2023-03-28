eb34dd5944c202eb6f27eaec9c9669485a5ee936
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2023-03-28 15:38:15 +0100

    Feat: Added payment service and responses

---
+++ Payment Gateway/Payment_Gateway.BLL/Infrastructure/SuccessResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_Gateway.BLL.Infrastructure
+{
+    public class SuccessResponse
+    {
+        public bool Success { get; set; }
+        public object Data { get; set; }
+    }
+
+    public class ErrorResponse
+    {
+        public int Status { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return JsonConvert.SerializeObject(this);
+        //}
+    }
+}
