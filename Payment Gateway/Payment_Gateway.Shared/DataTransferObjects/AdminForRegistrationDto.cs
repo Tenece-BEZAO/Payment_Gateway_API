a8f7be6df2d43b3fcbd656998ace6928f03bfa9f
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2023-03-30 09:42:49 +0100

    Feat: Added dtos

---
+++ Payment Gateway/Payment_Gateway.Shared/DataTransferObjects/AdminForRegistrationDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_Gateway.Shared.DataTransferObjects
+{
+    public class AdminForRegistrationDto
+    {
+        [Required(ErrorMessage = "Firstname is required")]
+        public string FirstName { get; init; }
+
+        [Required(ErrorMessage = "Lastname is required")]
+        public string LastName { get; init; }
+
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; init; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; init; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; init; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        public string PhoneNumber { get; init; }
+    }
+
+}
--- Payment Gateway/Payment_Gateway.Shared/DataTransferObjects/PaymentRequestDto.cs
+++ Payment Gateway/Payment_Gateway.Shared/DataTransferObjects/PaymentRequestDto.cs
@@ -1,13 +1,13 @@
-﻿namespace Payment_Gateway.BLL.Interfaces
-{
-    public class PaymentRequestDto
-    {
-        public decimal Amount { get; set; }
-        public string Email { get; set; }
-        public string Reference { get; set; }
-        public string Currency { get; set; }
-        public object Metadata { get; set; }
-        public string CallbackUrl { get; set; }
-    }
+﻿//namespace Payment_Gateway.BLL.Interfaces
+//{
+//    public class PaymentRequestDto
+//    {
+//        public decimal Amount { get; set; }
+//        public string Email { get; set; }
+//        public string Reference { get; set; }
+//        public string Currency { get; set; }
+//        public object Metadata { get; set; }
+//        public string CallbackUrl { get; set; }
+//    }
 
-}
+//}
