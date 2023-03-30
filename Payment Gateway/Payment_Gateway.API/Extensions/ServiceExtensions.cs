027eb5a80133ed52c815b26526a45eda1b0869c9
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2023-03-30 09:43:57 +0100

    Feat: Added some extensions and filters

--- Payment Gateway/Payment_Gateway.API/Extensions/ServiceExtensions.cs
+++ Payment Gateway/Payment_Gateway.API/Extensions/ServiceExtensions.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Payment_Gateway.BLL.LoggerService.Implementation;
 using Payment_Gateway.BLL.LoggerService.Interface;
 using Payment_Gateway.DAL.Context;
+using Payment_Gateway.Models.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Payment_Gateway.BLL.Implementation.Services;
+using Payment_Gateway.BLL.Interfaces.IServices;
+using Payment_Gateway.BLL.Interfaces;
+using Payment_Gateway.BLL.Implementation;
 
 namespace Payment_Gateway.API.Extensions
 {
+
     public static class ServiceExtensions
     {
         //Allows all requests from all origins to be sent to our API
@@ -28,6 +38,59 @@ namespace Payment_Gateway.API.Extensions
         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
            services.AddDbContext<PaymentGatewayDbContext>(opts =>
            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentity<User, IdentityRole>(o =>
+            {
+                o.Password.RequireDigit = true;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 10;
+                o.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<PaymentGatewayDbContext>()
+            .AddDefaultTokenProviders();
+        }
+        public static void ConfigureAuthenticationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+        }
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = Environment.GetEnvironmentVariable("SECRET");
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["validIssuer"],
+                    ValidAudience = jwtSettings["validAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+        }
+
+
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+           services.AddScoped<IUserServices, UserServices>();
+           services.AddScoped<IAdminServices, AdminServices>();
+           services.AddScoped<IAdminProfileServices, AdminProfileServices>();
+
+        }
 
     }
 }
