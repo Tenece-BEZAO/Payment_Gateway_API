88f054541522211e210b6e01bc6281bd131134e1
Victor Akwara <victorakwara83@gmail.com>
2023-04-05 12:57:27 +0100

    Feat: Added Authentication Response and RoleResponse classes

---
+++ Payment Gateway/Payment_Gateway.Shared/DataTransferObjects/Response/AuthenticationResponse.cs
@@ -0,0 +1,24 @@
+﻿using Payment_Gateway.Models.Entities;
+
+namespace Payment_Gateway.Shared.DataTransferObjects.Responses
+{
+    public class AuthenticationResponse
+    {
+        public JwtToken JwtToken { get; set; }
+        public string UserType { get; set; }
+        public string FullName { get; set; }
+        public IEnumerable<string> MenuItems { get; set; }
+        public bool? Birthday { get; set; }
+        public bool TwoFactor { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string ImpersonatorUsername { get; set; }
+        public bool IsImpersonating { get; set; }
+    }
+
+    public class JwtToken
+    {
+        public string Token { get; set; }
+        public DateTime Issued { get; set; }
+        public DateTime? Expires { get; set; }
+    }
+}
