b9c5200f3ca5e3fb6adf3dac0ae443aa81b5c9d5
Victor Akwara <victorakwara83@gmail.com>
2023-04-05 12:24:45 +0100

    Feat: Added AppConstants and Authorization Requirements classes

---
+++ Payment Gateway/Payment_Gateway.BLL/Infrastructure/AppConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_Gateway.BLL.Infrastructure
+{
+    public class AppConstants
+    {
+        public string SendGridAPIKey { get; set; }
+        public string AppURL { get; set; }
+        public string AppEmail { get; set; }
+        public string AppAcronym { get; set; }
+
+    }
+}
