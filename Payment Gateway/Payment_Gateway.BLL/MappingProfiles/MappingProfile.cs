a4f65e80c7e516bd10d2822da0ab41eba332d5f2
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2023-04-11 03:47:21 +0100

    modification

--- Payment Gateway/Payment_Gateway.BLL/MappingProfiles/MappingProfile.cs
+++ Payment Gateway/Payment_Gateway.BLL/MappingProfiles/MappingProfile.cs
@@ -11,15 +11,6 @@ namespace Payment_Gateway.BLL.MappingProfiles
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile()
-        {
-            CreateMap<Transaction, TransactionDto>()
-                .ForMember(dest => dest.Reference, opt => opt.MapFrom(src => src.TrxRef))
-                .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount))
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
-                .ForMember(dest => dest.TransactionDate, opt => opt.MapFrom(src => src.CreatedAt))
-                .ForMember(dest => dest.CustomerEmail, opt => opt.MapFrom(src => src.Email));
-        }
-
+       
     }
 }
