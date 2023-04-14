cf3500382d61a153e56f344f45ef469b995a8ab8
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2023-04-14 11:37:39 +0100

    modified

---
+++ Payment Gateway/Payment_Gateway.API/Controllers/WeatherAppController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Payment_Gateway.BLL.LoggerService.Implementation;
+
+namespace Payment_Gateway.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoggerController : ControllerBase
+    {
+
+
+        private ILoggerManager _logger;
+
+
+        public LoggerController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is info message from our values controller.");
+            _logger.LogDebug("Here is debug message from our values controller.");
+            _logger.LogWarn("Here is warn message from our values controller.");
+            _logger.LogError("Here is an error message from our values controller.");
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
